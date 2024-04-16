e4714c4cfa3ede4a6bd5e81aa9cb843a5e3fa3b8
小野 冬美香 <k023g0049@g.neec.ac.jp>
2024-04-16 17:31:32 +0900

    03_01_確認課題

---
+++ DirectXGame/Player.h
@@ -0,0 +1,36 @@
+#pragma once
+#include "WorldTransform.h"
+#include "Model.h"
+
+/// <summary>
+/// 自キャラ
+/// </summary>
+class Player {
+public:
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	/// <param name="model">モデル</param>
+	/// <param name="textureHandle">テクスチャハンドル</param>
+	void Intialize(Model* model,uint32_t textureHandle);
+
+	/// <summary>
+	/// 更新
+	/// </summary>
+	void Update();
+
+	/// <summary>
+	/// 描画
+	/// </summary>
+	void Draw(ViewProjection&viewProjection);
+
+private:
+	// ワールド変換データ
+	WorldTransform worldTransform_;
+	// モデル
+	Model* model_ = nullptr;
+	// テクスチャハンドル
+	uint32_t textureHandle_ = 0u;
+
+
+};
